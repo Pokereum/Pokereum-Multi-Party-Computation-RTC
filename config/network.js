d9bd5982e9e951e58da80643f7797e0fd7bf7bab
adibas03 <adibas03@ymail.com>
2016-12-10 03:42:47 +0100

    Updated IP of router

--- config/network.js
+++ config/network.js
@@ -2,7 +2,7 @@ module.exports = {
     publicIp: {host: "api.ipify.org", port: 80},
     telehashRouters: [
         "http://45.55.231.63:48484/",
-        "http://104.236.19.73:48484/"
+        "http://67.205.160.17:48484/"
     ],
     ethereumRpc: 'http://localhost:8545',
 };
